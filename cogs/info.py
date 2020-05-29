57584515dbf7a9d7fdb8783d1a626af57e389260
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-05-29 12:21:46 -0400

    Added Uptime comamnd

--- cogs/info.py
+++ cogs/info.py
@@ -17,13 +17,21 @@ class Info(commands.Cog):
             end = time.perf_counter()
             duration = (end - start) * 1000
             await message.edit(content='Pong! {:.2f}ms'.format(duration))
+            
+         @commands.command()
+        async def Uptime(self, ctx):
+        delta_uptime = datetime.utcnow() - self.bot.launch_time
+        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        await ctx.send(f"{days}d, {hours}h, {minutes}m, {seconds}s")
 
         @commands.group(invoke_without_command=True, name='About', description='About PyFeed Bot', case_insensitive=True)
         async def about_command(self, ctx):
-        embed = discord.Embed(title='PyFeed Bot', description=f'{self.bot.user.mention}')
-        embed.add_field(name='**About**',
-                        value=f'To be added to')
-        await ctx.send(embed=embed)
+            embed = discord.Embed(title='PyFeed Bot', description=f'{self.bot.user.mention}')
+            embed.add_field(name='**About**',
+                            value=f'To be added to')
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Info(bot))
