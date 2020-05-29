2f157cc3eb2b04e1ff95318bd84ac218c04eee00
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-05-29 04:46:15 -0400

    Created setup + class for Info Cog

--- cogs/info.py
+++ cogs/info.py
@@ -1,1 +1,15 @@
+import discord
+import datetime
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
 
+
+class Info(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+def setup(bot):
+    bot.add_cog(Info(bot))
