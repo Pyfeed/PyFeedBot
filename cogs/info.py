851dde7b52d067a1754a22cac94a351a7495cfd5
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-05-29 12:20:36 -0400

    Added about command group

--- cogs/info.py
+++ cogs/info.py
@@ -18,6 +18,12 @@ class Info(commands.Cog):
             duration = (end - start) * 1000
             await message.edit(content='Pong! {:.2f}ms'.format(duration))
 
+        @commands.group(invoke_without_command=True, name='About', description='About PyFeed Bot', case_insensitive=True)
+        async def about_command(self, ctx):
+        embed = discord.Embed(title='PyFeed Bot', description=f'{self.bot.user.mention}')
+        embed.add_field(name='**About**',
+                        value=f'To be added to')
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Info(bot))
