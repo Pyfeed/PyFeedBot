8c821847e1511410d785aaed55deaa0ca1b264a6
Ali <61859768+treehousekingcomic@users.noreply.github.com>
2020-05-29 12:15:44 -0400

    Added ping command [<#715768243553304623>]

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,6 @@
 import discord
 import datetime
+import time
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import asyncio
@@ -9,6 +10,13 @@ class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        @bot.command()
+        async def ping(self, ctx):
+            start = time.perf_counter()
+            message = await ctx.send("Ping...")
+            end = time.perf_counter()
+            duration = (end - start) * 1000
+            await message.edit(content='Pong! {:.2f}ms'.format(duration))
 
 
 def setup(bot):
